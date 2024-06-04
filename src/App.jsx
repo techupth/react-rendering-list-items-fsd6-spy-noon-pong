1f36a495eeb45bc1890fab18b64d8f9dbe95eeb3
Pong <pongpong34756@gmail.com>
2024-06-04 18:40:26 +0700

    not done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import "./App.css";
+import movies from "./data/movies";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        {" "}
+        Movie List Section
+        {movies.map((movie) => {
+          return (
+            <div className="moviecard">
+              <div className="image">
+                <img src={movie.image} />
+              </div>
+              <div className="detail">
+                Title: {movie.title}
+                <br></br>
+                year: {movie.year}
+                <br></br>
+                runtime: {movie.runtime}
+                <br></br>
+                genres:
+                {movie.map((index) => {
+                  return index.genres;
+                })}
+                <br></br>
+                imdbRating: {movie.imdbRating}
+                <br></br>
+                imdbVotes: {movie.imdbVotes}
+              </div>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
